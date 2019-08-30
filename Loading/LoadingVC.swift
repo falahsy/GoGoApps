715cc6bd9e967c193a87ed785510191ae790c500
mazmik25 <m.azmi65@yahoo.com>
2019-08-30 10:12:21 +0700

    silent error

--- Loading/LoadingVC.swift
+++ Loading/LoadingVC.swift
@@ -41,135 +41,133 @@ class LoadingVC: UIViewController {
     }
 
     private func observeWatch() {
-        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { (time) in
-            print("Background")
-            let kegiatan = Activity()
-            
-            self.checkSOSMessage(activity:kegiatan)
+//        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { (time) in
+//            print("Background")
+//            let kegiatan = Activity()
+        
+//            checkSOSMessage(activity: kegiatan)
             
             //MARK:- Real Time Location
             //send real time location
-            let user = User()
+//            let user = User()
             
-            self.sendRealtimeLocation(user: user)
+//            sendRealtimeLocation(user: user)
             //MARK:- Get Friends Location
-            self.mapView.removeAnnotations(self.friends)
+//            mapView.removeAnnotations(friends)
             
-            self.sortedFriendsByDistance = []
+//            sortedFriendsByDistance = []
             
-            user.searchActivity(activity: self.activityID ?? "", callback: { (users) in
-                
-                self.friends = []
-                for cyclist in users{
-                    
-                    if cyclist.userID != self.userID{
-                        self.friends.append(Cyclist(user: cyclist)!)
-                    }
-                    
-                }
-                
+//            user.searchActivity(activity: activityID ?? "", callback: { (users) in
+//                self.friends = []
+//                for cyclist in users {
+//                    if cyclist.userID != self.userID{
+//                        self.friends.append(Cyclist(user: cyclist)!)
+//                    }
+//
+//                }
+            
                 //MARK:- Detect distance between check point in route
-                var point = 0
-                for (index, element) in self.routesPoints.enumerated() {
-                    
-                    guard let distance = element.location?.distance(from: CLLocation(latitude: self.initialLocation.latitude, longitude: self.initialLocation.longitude)) else {
-                        return
-                    }
-                    
-                    if distance < 50 {
-                        point = index
-                    }
-                }
-                
-                var you:User!
-                
-                user.searchUser(userID: self.userID!) { (users) in
-                    users.first?.ref?.updateChildValues(["point": point])
-                    you = users.first!
-                    you.fullName = "You"
-                    self.sortedFriendsByDistance.append(you)
+//                var point = 0
+//                for (index, element) in self.routesPoints.enumerated() {
+//
+//                    guard let distance = element.location?.distance(from: CLLocation(latitude: self.initialLocation.latitude, longitude: self.initialLocation.longitude)) else {
+//                        return
+//                    }
+//
+//                    if distance < 50 {
+//                        point = index
+//                    }
+//                }
+            
+//                var you:User!
+//
+//                user.searchUser(userID: self.userID!) { (users) in
+//                    users.first?.ref?.updateChildValues(["point": point])
+//                    you = users.first!
+//                    you.fullName = "You"
+//                    self.sortedFriendsByDistance.append(you)
                     //just in case there are no more friends afterwards
                     //self.collectionView.reloadData()
                     
-                }
-                
-                //MARK:- calculating distance to destination
-                let checkPointPassed = self.routesPoints[point...]
-                //nedd to do this because the array slice cannot automatically converted
-                let checkDistance = Array(checkPointPassed)
-                self.mapView.removeOverlays(self.mapView.overlays)
-                self.drawRoutes(routes: checkDistance, draw: true)
-                
-                //MARK:- calculating friends distance to destination
-                for friend in  self.friends{
-                    user.searchUser(userID: friend.userID, callback: { (users) in
-                        self.friendsDistance[users.first!.userID] = users.first!.distance
-                    })
-                }
-                
-                //MARK:- Grouping all cyclists
-                self.groups = []
-                while self.friendsDistance.count > 0 {
-                    
-                    let sortedByValueDictionary = self.friendsDistance.sorted { $0.1 < $1.1 }
-                    guard let initialDistance = sortedByValueDictionary.first?.value else { return }
-                    
-                    let filteredDict = self.friendsDistance.filter{
-                        Int(($0.value - initialDistance)) < (50 )
-                    }
-                    
-                    if filteredDict.count > 0{
-                        
-                        self.groups.append(filteredDict)
-                        
-                        for cyclist in filteredDict{
-                            self.friendsDistance.removeValue(forKey: cyclist.key)
-                        }
-                        
-                    }
-                    
-                    
-                }
-                
-                self.friends = []
-                
-                for frontCyclist in self.groups{
-                    
-                    let sortedCyclistInGroup = frontCyclist.sorted { $0.1 < $1.1 }
-                    
-                    user.searchUser(userID: sortedCyclistInGroup.first!.key, callback: { (users) in
-                        
-                        var info = ""
-                        var detail = ""
-                        var user = users.first!
-                        
-                        if frontCyclist.count > 1 {
-                            info = "consists of \(frontCyclist.count ) friends"
-                            detail = "\(users.first!.fullName) is in group"
-                            user.fullName = info
-                            
-                        }else{
-                            info = users.first?.fullName ?? ""
-                            detail = users.first?.userID ?? ""
-                        }
-                        self.friends.append(Cyclist(fullname: info, userID: detail, discipline: "Flag", coordinate: users.first!.location))
-                        
-                        self.mapView.addAnnotations(self.friends)
-                        
-                        self.sortedFriendsByDistance.append(user)
-                        
-                        self.sortedFriendsByDistance = self.sortedFriendsByDistance.sorted(by: { (user1, user2) -> Bool in return user1.distance < user2.distance
-                        })
-                        
-                        self.collectionView.reloadData()
-                    })
-                }
-                
-                self.collectionView.reloadData()
-                
-                
-            })
+//                }
             
-        }
+                //MARK:- calculating distance to destination
+//                let checkPointPassed = self.routesPoints[point...]
+                //nedd to do this because the array slice cannot automatically converted
+//                let checkDistance = Array(checkPointPassed)
+//                self.mapView.removeOverlays(self.mapView.overlays)
+//                self.drawRoutes(routes: checkDistance, draw: true)
+            
+                //MARK:- calculating friends distance to destination
+//                for friend in  self.friends{
+//                    user.searchUser(userID: friend.userID, callback: { (users) in
+//                        self.friendsDistance[users.first!.userID] = users.first!.distance
+//                    })
+//                }
+            
+                //MARK:- Grouping all cyclists
+//                self.groups = []
+//                while self.friendsDistance.count > 0 {
+//
+//                    let sortedByValueDictionary = self.friendsDistance.sorted { $0.1 < $1.1 }
+//                    guard let initialDistance = sortedByValueDictionary.first?.value else { return }
+//
+//                    let filteredDict = self.friendsDistance.filter{
+//                        Int(($0.value - initialDistance)) < (50 )
+//                    }
+//
+//                    if filteredDict.count > 0{
+//
+//                        self.groups.append(filteredDict)
+//
+//                        for cyclist in filteredDict{
+//                            self.friendsDistance.removeValue(forKey: cyclist.key)
+//                        }
+//
+//                    }
+//
+//
+//                }
+            
+//                self.friends = []
+//
+//                for frontCyclist in self.groups {
+//
+//                    let sortedCyclistInGroup = frontCyclist.sorted { $0.1 < $1.1 }
+//
+//                    user.searchUser(userID: sortedCyclistInGroup.first!.key, callback: { (users) in
+//
+//                        var info = ""
+//                        var detail = ""
+//                        var user = users.first!
+//
+//                        if frontCyclist.count > 1 {
+//                            info = "consists of \(frontCyclist.count ) friends"
+//                            detail = "\(users.first!.fullName) is in group"
+//                            user.fullName = info
+//
+//                        }else{
+//                            info = users.first?.fullName ?? ""
+//                            detail = users.first?.userID ?? ""
+//                        }
+//                        self.friends.append(Cyclist(fullname: info, userID: detail, discipline: "Flag", coordinate: users.first!.location))
+//
+//                        self.mapView.addAnnotations(self.friends)
+//
+//                        self.sortedFriendsByDistance.append(user)
+//
+//                        self.sortedFriendsByDistance = self.sortedFriendsByDistance.sorted(by: { (user1, user2) -> Bool in return user1.distance < user2.distance
+//                        })
+//
+//                        self.collectionView.reloadData()
+//                    })
+//                }
+//
+//                self.collectionView.reloadData()
+            
+                
+//            })
+            
+//        }
     }
 }
