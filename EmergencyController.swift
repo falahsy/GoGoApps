aca814418d93dce5aa59a37e4d5c594a865ee087
Andika Leonardo <leonardodika.13@gmail.com>
2019-09-03 15:25:56 +0700

    UI-done -> Doing List Validation

---
+++ EmergencyController.swift
@@ -0,0 +1,13 @@
+//
+//  EmergencyController.swift
+//  
+//
+//  Created by Andika Leonardo on 03/09/19.
+//
+
+import UIKit
+import Watchkit
+
+class EmergencyController: WKInterfaceController {
+
+}
---
+++ gogoApps Extension/Watch Controller/EmergencyController.swift
@@ -0,0 +1,14 @@
+//
+//  EmergencyController.swift
+//  gogoApps Extension
+//
+//  Created by Andika Leonardo on 03/09/19.
+//  Copyright © 2019 boy setiawan. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class EmergencyController: WKInterfaceController {
+
+}
