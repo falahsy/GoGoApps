50f325f68c7f558eee2df0084449814fa9991526
ferryirawan <27ferryirawan@gmail.com>
2019-09-04 12:23:33 +0700

    home to event detail

--- Model/UpcomingEventInfo.swift
+++ Model/UpcomingEventInfo.swift
@@ -15,13 +15,17 @@ struct UpcomingEventInfo {
     var destination:String
     var distance:Double
     var eta:Int
+    var row:Int
+    var isToday:Bool
     
-    init(activityID:String, date:Date, friends: Int, destination:String, distance:Double,eta:Int) {
+    init(activityID:String, date:Date, friends: Int, destination:String, distance:Double,eta:Int, row:Int, isToday:Bool) {
         self.activityID = activityID
         self.date = date
         self.friends = friends
         self.destination = destination
         self.distance = distance
         self.eta = eta
+        self.row = row
+        self.isToday = isToday
     }
 }
